2583e5640f225212b0d5451336132ff877434ab9
WSobczyk <wiktoriasobczyk@gmail.com>
2024-06-16 23:07:14 +0200

    feat: Basic sentry integration

---
+++ lib/app/app.dart
@@ -0,0 +1,60 @@
+import 'package:feedback/feedback.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BetterFeedback(
+      child: MaterialApp(
+        title: 'Flutter Feedback Example',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Flutter Feedback Example'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Example of feedback',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          BetterFeedback.of(context).show((UserFeedback feedback) {
+            /// Send feedback to your server
+          });
+        },
+        tooltip: 'Feedback',
+        child: const Icon(Icons.feedback),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,28 @@
-import 'package:feedback/feedback.dart';
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_feedback_example/app/app.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final sentryDns = await _receiveSentryDns();
+
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = sentryDns;
+      options.tracesSampleRate = 1.0;
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(const MyApp()),
+  );
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<String> _receiveSentryDns() async {
+  final String json = await rootBundle.loadString('keys/keys.json');
 
-  @override
-  Widget build(BuildContext context) {
-    return BetterFeedback(
-      child: MaterialApp(
-        title: 'Flutter Feedback Example',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage(title: 'Flutter Feedback Example'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Example of feedback',
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          BetterFeedback.of(context).show((UserFeedback feedback) {
-            /// Send feedback to your server
-          });
-        },
-        tooltip: 'Feedback',
-        child: const Icon(Icons.feedback),
-      ),
-    );
-  }
+  final Map<String, dynamic> config = jsonDecode(json);
+  return config['sentry_dns'] as String;
 }
